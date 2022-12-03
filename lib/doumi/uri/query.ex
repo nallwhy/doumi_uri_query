53e98a93053da0d61a76f42c352291d73c11c1cd
Jinkyou Son <nallwhy@gmail.com>
2022-12-03 12:02:22 +0900

    Implement Doumi.URI.Query

--- lib/doumi/uri/query.ex
+++ lib/doumi/uri/query.ex
@@ -1,18 +1,76 @@
 defmodule Doumi.URI.Query do
-  @moduledoc """
-  Documentation for `Doumi.URI.Query`.
-  """
-
   @doc """
-  Hello world.
+  Encodes `map_or_keyword` into a query string using `encoding`.
+
+  Compared to `URI.encode_query/2`, `Doumi.URI.Query.encode/2` can encode nested maps and lists.
+
+  `encoding` follows the same rules as `URI.encode_query/2`.
 
   ## Examples
 
-      iex> Doumi.URI.Query.hello()
-      :world
+      iex> query = %{"foo" => 1, "bar" => 2}
+      iex> Doumi.URI.Query.encode(query)
+      "bar=2&foo=1"
+
+      iex> query = [foo: 1, bar: 2]
+      iex> Doumi.URI.Query.encode(query)
+      "bar=2&foo=1"
+
+      iex> query = %{"foo" => %{"bar" => 1, "baz" => 2}}
+      iex> Doumi.URI.Query.encode(query)
+      "foo%5Bbar%5D=1&foo%5Bbaz%5D=2"
+
+      iex> query = %{"foo" => [1, 2, 3]}
+      iex> Doumi.URI.Query.encode(query)
+      "foo%5B%5D=1&foo%5B%5D=2&foo%5B%5D=3"
 
   """
-  def hello do
-    :world
+
+  @spec encode(map() | keyword(), :rfc3986 | :www_form) :: String.t()
+  def encode(map_or_keyword, encoding \\ :www_form)
+
+  def encode(map, encoding) when is_map(map) do
+    to_params(map, [])
+    |> List.flatten()
+    |> URI.encode_query(encoding)
+  end
+
+  def encode(maybe_enumerable, encoding) do
+    case Keyword.keyword?(maybe_enumerable) do
+      true ->
+        keyword = maybe_enumerable
+        encode(Map.new(keyword), encoding)
+
+      false ->
+        raise ArgumentError, "expected a map or keyword list, got: #{inspect(maybe_enumerable)}"
+    end
+  end
+
+  defp to_params(map, context) when is_map(map) do
+    map
+    |> Enum.map(fn {key, value} ->
+      to_params(value, [key | context])
+    end)
+  end
+
+  defp to_params(list, context) when is_list(list) do
+    list
+    |> Enum.map(fn value ->
+      to_params(value, ["" | context])
+    end)
+  end
+
+  defp to_params(value, context) do
+    {to_key(context), to_string(value)}
+  end
+
+  defp to_key(context) do
+    [h | t] = context |> Enum.reverse()
+
+    h_key = to_string(h)
+    t_keys = t |> Enum.map(&"[#{&1}]")
+
+    [h_key | t_keys]
+    |> IO.iodata_to_binary()
   end
 end
--- test/doumi/uri/query_test.exs
+++ test/doumi/uri/query_test.exs
@@ -2,7 +2,16 @@ defmodule Doumi.URI.QueryTest do
   use ExUnit.Case
   doctest Doumi.URI.Query
 
-  test "greets the world" do
-    assert Doumi.URI.Query.hello() == :world
+  test "throw ArgumentError when given not map or keyword list" do
+    assert_raise ArgumentError, ~r"expected a map or keyword list", fn ->
+      Doumi.URI.Query.encode([1, 2, 3])
+    end
+  end
+
+  test "should returns the same result of URI.encode_query/2" do
+    query = %{"key" => "value with spaces"}
+
+    assert Doumi.URI.Query.encode(query) == URI.encode_query(query)
+    assert Doumi.URI.Query.encode(query, :rfc3986) == URI.encode_query(query, :rfc3986)
   end
 end
