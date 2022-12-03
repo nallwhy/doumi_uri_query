26c481b8dd9ca190066df554d4d8a52e8ab8b131
Jinkyou Son <nallwhy@gmail.com>
2022-12-03 10:19:02 +0900

    Init project

---
+++ lib/doumi/uri/query.ex
@@ -0,0 +1,18 @@
+defmodule Doumi.URI.Query do
+  @moduledoc """
+  Documentation for `Doumi.URI.Query`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Doumi.URI.Query.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/doumi/uri/query_test.exs
@@ -0,0 +1,8 @@
+defmodule Doumi.URI.QueryTest do
+  use ExUnit.Case
+  doctest Doumi.URI.Query
+
+  test "greets the world" do
+    assert Doumi.URI.Query.hello() == :world
+  end
+end
